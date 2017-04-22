5e1f3255cd59b8d08470671fd6f58864d9f065d5
ligi <ligi@ligi.de>
2017-04-22 23:29:32 +0200

    Add UI-Test for InfoActivity

---
+++ app/src/androidTest/java/org/walleth/TheInfoActivity.kt
@@ -0,0 +1,25 @@
+package org.walleth
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Rule
+import org.junit.Test
+import org.ligi.trulesk.TruleskActivityRule
+import org.ligi.walleth.R
+import org.ligi.walleth.activities.InfoActivity
+
+class TheInfoActivity {
+
+    @get:Rule
+    var rule = TruleskActivityRule(InfoActivity::class.java)
+
+    @Test
+    fun infoShows() {
+        onView(withId(R.id.intro_text)).check(matches(isDisplayed()))
+
+        rule.screenShot("info")
+    }
+
+}
