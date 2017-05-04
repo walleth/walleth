17c843af1974de5c9c31c409d04d412c5af56e14
ligi <ligi@ligi.de>
2017-05-05 00:21:06 +0200

    Set note to gone if not present

--- app/src/main/java/org/walleth/ui/AddressViewHolder.kt
+++ app/src/main/java/org/walleth/ui/AddressViewHolder.kt
@@ -2,17 +2,28 @@ package org.walleth.ui
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import kotlinx.android.synthetic.main.item_address_book.view.*
 import org.walleth.data.addressbook.AddressBookEntry
 
 class AddressViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(addressBookEntry: AddressBookEntry, onClickAction: (entry: AddressBookEntry) -> Unit) {
+
         itemView.setOnClickListener {
             onClickAction.invoke(addressBookEntry)
         }
+
         itemView.address_name.text = addressBookEntry.name
-        itemView.address_note.text = addressBookEntry.note
+
+        if (addressBookEntry.note == null || addressBookEntry.note!!.isBlank()) {
+            itemView.address_note.visibility = GONE
+        } else {
+            itemView.address_note.visibility = VISIBLE
+            itemView.address_note.text = addressBookEntry.note
+        }
+
         itemView.address_hash.text = addressBookEntry.address.hex
     }
 
