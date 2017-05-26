c2bdc7bc3c9680f79e3a742fca46743d02a69b9e
ligi <ligi@ligi.de>
2017-05-26 12:44:50 +0200

    Add ability to print your private key to act as a paper wallet

--- app/src/main/java/org/walleth/activities/ExportKeyActivity.kt
+++ app/src/main/java/org/walleth/activities/ExportKeyActivity.kt
@@ -1,12 +1,21 @@
 package org.walleth.activities
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Point
+import android.os.Build
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.github.salomonbrys.kodein.LazyKodein
 import com.github.salomonbrys.kodein.android.appKodein
 import com.github.salomonbrys.kodein.instance
@@ -16,6 +25,7 @@ import org.ligi.kaxt.doAfterEdit
 import org.ligi.kaxt.setVisibility
 import org.walleth.R
 import org.walleth.data.keystore.WallethKeyStore
+import java.io.ByteArrayOutputStream
 
 
 class ExportKeyActivity : AppCompatActivity() {
@@ -50,8 +60,9 @@ class ExportKeyActivity : AppCompatActivity() {
         qrcode_image.setImageBitmap(bmpScaled)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_export, menu)
+        menu.findItem(R.id.menu_print).isVisible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -71,6 +82,50 @@ class ExportKeyActivity : AppCompatActivity() {
             startActivity(sendIntent)
             true
         }
+
+        R.id.menu_print -> {
+            doWebViewPrint()
+            true
+        }
         else -> super.onOptionsItemSelected(item)
     }
+
+    var mWebView: WebView? = null
+    private fun doWebViewPrint() {
+        val webView = WebView(this)
+        webView.setWebViewClient(object : WebViewClient() {
+
+            @SuppressLint("NewApi") // we hide the menu entry for SDK < 19 so they *should* never land here
+            override fun onPageFinished(view: WebView, url: String) {
+                createWebPrintJob(view)
+                mWebView = null
+            }
+        })
+
+        val bos = ByteArrayOutputStream()
+        val qrCode = QRCode.from(keyJSON)
+        qrCode.bitmap().compress(Bitmap.CompressFormat.PNG, 100, bos)
+        bos.close()
+        val encode = Base64.encodeToString(bos.toByteArray(), Base64.DEFAULT)
+
+        // Generate an HTML document on the fly:
+        val htmlDocument = "<html><body><h1>" + getString(R.string.paper_wallet_title)+"</h1>" +
+                "<p><font size='21'>"+getString(R.string.paper_wallet_text)+"</font></p>" +
+                "<center><img width=\"80%\" src=\"data:image/png;base64,$encode\"/></center></body></html>"
+        webView.loadDataWithBaseURL(null, htmlDocument, "text/HTML", "UTF-8", null)
+
+        mWebView = webView
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    private fun createWebPrintJob(webView: WebView) {
+
+        val printAdapter = webView.createPrintDocumentAdapter()
+
+        val jobName = getString(R.string.app_name) + " Document"
+        val printManager = getSystemService(Context.PRINT_SERVICE) as PrintManager
+        printManager.print(jobName, printAdapter, PrintAttributes.Builder().build())
+
+    }
+
 }
