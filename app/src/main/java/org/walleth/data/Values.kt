a9bba0d95c058cb66ddae8da99833ce8fb90c7d3
ligi <ligi@ligi.de>
2017-06-26 23:44:53 +0200

    Increase the default gasPrice to 20gwei

--- app/src/main/java/org/walleth/data/Values.kt
+++ app/src/main/java/org/walleth/data/Values.kt
@@ -4,7 +4,7 @@ import java.math.BigInteger
 
 val ETH_IN_WEI = BigInteger("1000000000000000000")
 
-var DEFAULT_GAS_PRICE = BigInteger("2000000000")
+var DEFAULT_GAS_PRICE = BigInteger("20000000000")
 var DEFAULT_GAS_LIMIT_ETH_TX = BigInteger("21000")
 var DEFAULT_GAS_LIMIT_ERC_20_TX = BigInteger("73000")
 
