c23a5ceda820004c8eb785df629fc4850a43058d
ligi <ligi@ligi.de>
2017-07-02 19:45:31 +0200

    Fix flaky tests

--- app/src/androidTest/java/org/walleth/TheCreateAccountActivity.kt
+++ app/src/androidTest/java/org/walleth/TheCreateAccountActivity.kt
@@ -1,6 +1,5 @@
 package org.walleth
 
-import android.support.test.espresso.Espresso.closeSoftKeyboard
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
@@ -35,11 +34,9 @@ class TheCreateAccountActivity {
     @Test
     fun rejects_blank_name() {
 
-        onView(withId(R.id.hexInput)).perform(scrollTo(),typeText("0xfdf1210fc262c73d0436236a0e07be419babbbc4"))
+        onView(withId(R.id.hexInput)).perform(scrollTo(), typeText("0xfdf1210fc262c73d0436236a0e07be419babbbc4"))
 
-        closeSoftKeyboard()
-
-        onView(withId(R.id.fab)).perform(click())
+        onView(withId(R.id.fab)).perform(closeSoftKeyboard(), click())
 
         onView(withText(R.string.alert_problem_title)).check(matches(isDisplayed()))
         onView(withText(R.string.please_enter_name)).check(matches(isDisplayed()))
@@ -51,21 +48,20 @@ class TheCreateAccountActivity {
     @Test
     fun when_creating_new_address_old_gets_cleared() {
 
-        onView(withId(R.id.new_address_button)).perform(click())
+        onView(withId(R.id.new_address_button)).perform(closeSoftKeyboard(), click())
 
         val firstCreatedAddress = Address(rule.activity.hexInput.text.toString())
 
+        assertThat(firstCreatedAddress.hex).startsWith("0x")
         assertThat(TestApp.keyStore.hasKeyForForAddress(firstCreatedAddress)).isTrue()
 
         onView(withId(R.id.new_address_button)).perform(click())
 
         val secondCreatedAddress = Address(rule.activity.hexInput.text.toString())
 
-        onView(withId(R.id.nameInput)).perform(scrollTo(),typeText("nameProbe"))
+        onView(withId(R.id.nameInput)).perform(scrollTo(), typeText("nameProbe"))
 
-        closeSoftKeyboard()
-
-        onView(withId(R.id.fab)).perform(click())
+        onView(withId(R.id.fab)).perform(closeSoftKeyboard(), click())
 
         assertThat(TestApp.keyStore.hasKeyForForAddress(firstCreatedAddress)).isFalse()
         assertThat(TestApp.keyStore.hasKeyForForAddress(secondCreatedAddress)).isTrue()
@@ -75,15 +71,14 @@ class TheCreateAccountActivity {
     @Test
     fun savesValidAddress() {
 
-        onView(withId(R.id.hexInput)).perform(scrollTo(),typeText("0xfdf1210fc262c73d0436236a0e07be419babbbc4"))
-        onView(withId(R.id.nameInput)).perform(scrollTo(),typeText("nameProbe"))
-        onView(withId(R.id.noteInput)).perform(scrollTo(),typeText("noteProbe"))
+        onView(withId(R.id.hexInput)).perform(scrollTo(), typeText("0xfdf1210fc262c73d0436236a0e07be419babbbc4"))
+        onView(withId(R.id.nameInput)).perform(scrollTo(), typeText("nameProbe"))
+        onView(withId(R.id.noteInput)).perform(scrollTo(), typeText("noteProbe"))
 
-        closeSoftKeyboard()
 
         rule.screenShot("create")
 
-        onView(withId(R.id.fab)).perform(click())
+        onView(withId(R.id.fab)).perform(closeSoftKeyboard(), click())
 
         val tested = TestApp.addressBookWithEntries.getEntryForName(Address("0xfdf1210fc262c73d0436236a0e07be419babbbc4"))
 
@@ -92,4 +87,6 @@ class TheCreateAccountActivity {
         assertThat(tested.note).isEqualTo("noteProbe")
         assertThat(rule.activity.isFinishing).isTrue()
     }
+
+
 }
