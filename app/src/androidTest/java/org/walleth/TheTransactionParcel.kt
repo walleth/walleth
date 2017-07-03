a79ab4439dcaca4687713accf867c9b092b84183
ligi <ligi@ligi.de>
2017-07-03 23:38:03 +0200

    Add TransactionParcel

---
+++ app/src/androidTest/java/org/walleth/TheTransactionParcel.kt
@@ -0,0 +1,52 @@
+package org.walleth
+
+import android.os.Parcel
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.kethereum.model.Address
+import org.kethereum.model.Transaction
+import org.walleth.kethereum.android.TransactionParcel
+import java.math.BigInteger
+
+class TheTransactionParcel {
+
+    @Test
+    fun normalTransactionSurvives() {
+
+        val transactionBefore = Transaction(BigInteger("10"), Address("0xab"), Address("0xcd"), nonce = 42)
+        testTransactionParcel(transactionBefore)
+    }
+
+    @Test
+    fun customTransactionSurvives() {
+
+        val transactionBefore = Transaction(BigInteger("1000"), Address("0xab"), Address("0xcd"),
+                creationEpochSecond = 10L,
+                gasLimit = BigInteger("123"),
+                gasPrice = BigInteger("123542"),
+                input = listOf(1, 2, 3, 4, 2),
+                txHash = "0xfoo",
+                sigHash = "0xBar")
+        testTransactionParcel(transactionBefore)
+    }
+
+    @Test
+    fun transactionWithNullFieldsSurvives() {
+
+        val transactionBefore = Transaction(BigInteger("0"), Address("0xab"), null)
+        testTransactionParcel(transactionBefore)
+    }
+
+    private fun testTransactionParcel(transactionBefore: Transaction) {
+        val transactionParcel = TransactionParcel(transactionBefore)
+
+        val obtain = Parcel.obtain()
+        transactionParcel.writeToParcel(obtain, 0)
+
+        obtain.setDataPosition(0)
+
+        val transactionAfter = TransactionParcel.createFromParcel(obtain).transaction
+
+        assertThat(transactionAfter).isEqualTo(transactionBefore)
+    }
+}
---
+++ app/src/main/java/org/walleth/kethereum/android/TransactionParcel.kt
@@ -0,0 +1,44 @@
+package org.walleth.kethereum.android
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.kethereum.model.Address
+import org.kethereum.model.Transaction
+import java.math.BigInteger
+
+class TransactionParcel(val transaction: Transaction) : Parcelable {
+
+    constructor(parcel: Parcel) : this(Transaction(
+            value = BigInteger(parcel.readString()),
+            from = Address(parcel.readString()),
+            txHash = parcel.readValue(null) as String?,
+            sigHash = parcel.readValue(null) as String?,
+            to = (parcel.readValue(null) as String?)?.let { Address(it) },
+            nonce = parcel.readValue(null) as Long?,
+            creationEpochSecond = parcel.readValue(null) as Long?,
+            gasPrice = BigInteger(parcel.readString()),
+            gasLimit = BigInteger(parcel.readString()),
+            input = parcel.createByteArray().toList()))
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(transaction.value.toString())
+        dest.writeString(transaction.from.hex)
+        dest.writeValue(transaction.txHash)
+        dest.writeValue(transaction.sigHash)
+        dest.writeValue(transaction.to?.hex)
+        dest.writeValue(transaction.nonce)
+        dest.writeValue(transaction.creationEpochSecond)
+        dest.writeString(transaction.gasPrice.toString())
+        dest.writeString(transaction.gasLimit.toString())
+        dest.writeByteArray(transaction.input.toByteArray())
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<TransactionParcel> {
+        override fun createFromParcel(parcel: Parcel) = TransactionParcel(parcel)
+
+        override fun newArray(size: Int): Array<TransactionParcel?> = arrayOfNulls(size)
+    }
+
+}
