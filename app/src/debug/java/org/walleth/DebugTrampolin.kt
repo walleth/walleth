b47400374e368d043f359e264dd1752c303490cc
ligi <ligi@ligi.de>
2017-07-04 11:56:32 +0200

    Add DebugTrampolin

---
+++ app/src/debug/java/org/walleth/DebugTrampolin.kt
@@ -0,0 +1,16 @@
+package org.walleth
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.ligi.kaxt.startActivityFromClass
+import org.walleth.activities.AddressBookActivity
+
+class DebugTrampolin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivityFromClass(AddressBookActivity::class.java)
+        finish()
+    }
+}
