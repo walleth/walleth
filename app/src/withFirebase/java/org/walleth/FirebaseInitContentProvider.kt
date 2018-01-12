abcaaab93e795d58d739522f3f7e8cb71879081d
ligi <ligi@ligi.de>
2018-01-12 01:57:09 +0100

    Fix indent

--- app/src/withFirebase/java/org/walleth/FirebaseInitContentProvider.kt
+++ app/src/withFirebase/java/org/walleth/FirebaseInitContentProvider.kt
@@ -8,14 +8,14 @@ import android.net.Uri
 class FirebaseInitContentProvider : ContentProvider() {
 
     override fun onCreate(): Boolean {
-        context.startService(Intent(context,FirebaseIntentService::class.java))
+        context.startService(Intent(context, FirebaseIntentService::class.java))
         return true
     }
 
     override fun insert(uri: Uri?, values: ContentValues?) = null
     override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?) = null
-    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?) =0
-    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?) =0
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?) = 0
+    override fun delete(uri: Uri?, selection: String?, selectionArgs: Array<out String>?) = 0
     override fun getType(uri: Uri?) = null
 
 }
