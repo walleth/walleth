90ee158891b8b3dbe962b0f6b582714bbddfb7ea
ligi <ligi@ligi.de>
2018-04-16 01:09:42 +0200

    Update token definitions

--- app/src/main/java/org/walleth/data/TokenPrimer.kt
+++ app/src/main/java/org/walleth/data/TokenPrimer.kt
@@ -12,7 +12,7 @@ import org.walleth.data.networks.AllNetworkDefinitions
 import org.walleth.data.tokens.Token
 import org.walleth.data.tokens.getEthTokenForChain
 
-private const val TOKEN_INIT_VERSION = 10
+private const val TOKEN_INIT_VERSION = 11
 // yes this is opinionated - but it also cuts to the chase
 // so much garbage in this token-list ..
 
