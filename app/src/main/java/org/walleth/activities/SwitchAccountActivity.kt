4db621a823670d0bf995f9dc41c2d56ab679ecda
ligi <ligi@ligi.de>
2018-04-15 22:06:32 +0200

    Add create transaction shortcut

--- app/src/main/java/org/walleth/activities/SwitchAccountActivity.kt
+++ app/src/main/java/org/walleth/activities/SwitchAccountActivity.kt
@@ -8,7 +8,7 @@ class SwitchAccountActivity : BaseAddressBookActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.subtitle = getString(R.string.switch_account_subtitle)
+        supportActionBar?.subtitle = getString(R.string.nav_drawer_accounts)
     }
 
     override fun onAddressClick(addressEntry: AddressBookEntry) {
