60026bfafa82e4fea261653b462a711d5317bd47
ligi <ligi@ligi.de>
2018-06-14 19:15:34 +0200

    Extract license displaying to a library - closes #245

--- app/src/main/java/org/walleth/activities/OpenSourceLicenseDisplayActivity.kt
+++ app/src/main/java/org/walleth/activities/OpenSourceLicenseDisplayActivity.kt
@@ -2,44 +2,16 @@ package org.walleth.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_list_simple.*
 import org.walleth.R
-import org.walleth.ui.info.licenses.LicenseInfoAdapter
-import org.walleth.ui.info.licenses.LicenseInfoEntry
 
 class OpenSourceLicenseDisplayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_list_simple)
-
-        supportActionBar?.run {
-            setDisplayHomeAsUpEnabled(true)
-            subtitle = getString(R.string.activity_subtitle_oss_license)
-        }
-
-        val metadataText = readResource("third_party_license_metadata")
-        val ins2 = metadataText.split("\n").filter { it.isNotBlank() }.map {
-            it.split(" ").let {
-                val positions = it.first().split(":")
-                LicenseInfoEntry(it[1], positions.first().toInt(), positions.last().toInt())
-            }
-        }
-
-
-        val data = readResource("third_party_licenses")
-
-        recycler_view.adapter = LicenseInfoAdapter(ins2, data)
-        recycler_view.layoutManager = LinearLayoutManager(this)
+        setContentView(R.layout.activity_license_info)
     }
-
-    private fun readResource(name: String) = resources.openRawResource(getIdentifier(name)).reader().readText()
-
-    private fun getIdentifier(name: String) = resources.getIdentifier(name, "raw", packageName)
-
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> true.also {
--- app/src/main/java/org/walleth/ui/info/licenses/LicenseInfoEntry.kt
+++
@@ -1,3 +0,0 @@
-package org.walleth.ui.info.licenses
-
-data class LicenseInfoEntry(val name: String, val position: Int, val length: Int)
