5fce0d2b010f6e2a3efa699c8b77b7a526fd5597
ligi <ligi@ligi.de>
2018-11-07 21:16:35 +0100

    Apply the new toolbar colors after returning from settings

--- app/src/main/java/org/walleth/activities/PreferenceActivity.kt
+++ app/src/main/java/org/walleth/activities/PreferenceActivity.kt
@@ -2,15 +2,29 @@ package org.walleth.activities
 
 
 import android.os.Bundle
+import org.ligi.kaxt.recreateWhenPossible
 import org.walleth.R
 
 
 class PreferenceActivity : BaseSubActivity() {
 
+    var firstResume = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_prefs)
+        firstResume = true
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        if (!firstResume) {
+            // to apply the new toolbar configuration
+            recreateWhenPossible()
+        }
+
+        firstResume = false
+    }
 }
