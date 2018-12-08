8c65018ac2eec2adae50dbc2ca5f364c6ec3af82
ligi <ligi@ligi.de>
2018-12-08 18:24:21 +0100

    Deal with invalid json - closes #317

--- app/src/main/java/org/walleth/walletconnect/WalletConnectFun.kt
+++ app/src/main/java/org/walleth/walletconnect/WalletConnectFun.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 import org.kethereum.erc681.ERC681
 import org.kethereum.erc681.generateURL
@@ -25,45 +26,53 @@ fun Context.createIntentForTransaction(statefulRPCCall: WalletConnectDriver.Stat
     val rpcCall = statefulRPCCall.call
     val params = JSONArray(rpcCall.paramsJSON)
 
-    if (rpcCall.method == "eth_sendTransaction") {
-        val transactionParameters = params.getJSONObject(0)
+    return when (rpcCall.method) {
+        "eth_sendTransaction" -> {
+            val transactionParameters = params.getJSONObject(0)
 
-        // TODO - check from
+            // TODO - check from
 
-        val url = ERC681(scheme = "ethereum",
-                address = transactionParameters.getString("to"),
-                value = BigInteger(transactionParameters.getString("value").clean0xPrefix(), 16),
-                gas = BigInteger(transactionParameters.getString("gasLimit").clean0xPrefix(), 16)
-        ).generateURL()
+            try {
+                val url = ERC681(scheme = "ethereum",
+                        address = transactionParameters.getString("to"),
+                        value = BigInteger(transactionParameters.getString("value").clean0xPrefix(), 16),
+                        gas = BigInteger(transactionParameters.getString("gasLimit").clean0xPrefix(), 16)
+                ).generateURL()
 
-        return Intent(this, WalletConnectSendTransactionActivity::class.java).apply {
-            data = Uri.parse(url)
-            setWalletConnectExtras(statefulRPCCall)
-            if (transactionParameters.has("data")) {
-                val string = transactionParameters.getString("data")
-                putExtra("data", string)
+                Intent(this, WalletConnectSendTransactionActivity::class.java).apply {
+                    data = Uri.parse(url)
+                    setWalletConnectExtras(statefulRPCCall)
+                    if (transactionParameters.has("data")) {
+                        val string = transactionParameters.getString("data")
+                        putExtra("data", string)
+                    }
+
+                    putExtra("gasPrice", transactionParameters.getString("gasPrice"))
+                    putExtra("nonce", transactionParameters.getString("nonce"))
+                    putExtra("from", transactionParameters.getString("to"))
+                    putExtra("parityFlow", false)
+                }
+            } catch (e: JSONException) {
+                createErrorIntent("Error: illegal JSON: " + e.message)
             }
-
-            putExtra("gasPrice", transactionParameters.getString("gasPrice"))
-            putExtra("nonce", transactionParameters.getString("nonce"))
-            putExtra("from", transactionParameters.getString("to"))
-            putExtra("parityFlow", false)
-        }
-    }
-    if (rpcCall.method == "eth_sign") {
-
-        return Intent(this, WalletConnectSignTextActivity::class.java).apply {
-            putExtra(Intent.EXTRA_TEXT, params.getString(1))
-
-            setWalletConnectExtras(statefulRPCCall)
         }
 
-    }
-    return Intent(this, WalletConnectErrorActivity::class.java).apply {
-        putExtra(Intent.EXTRA_TEXT, "Err unknown method: " + rpcCall.method)
-    }
+        "eth_sign" -> {
+            return Intent(this, WalletConnectSignTextActivity::class.java).apply {
+                putExtra(Intent.EXTRA_TEXT, params.getString(1))
 
+                setWalletConnectExtras(statefulRPCCall)
+            }
+        }
+
+        else -> createErrorIntent("Err unknown method: " + rpcCall.method)
+    }
 }
+
+private fun Context.createErrorIntent(text: String) =
+        Intent(this, WalletConnectErrorActivity::class.java).apply {
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
 
 private fun Intent.setWalletConnectExtras(statefulRPCCall: WalletConnectDriver.StatefulJSONRPCCall) {
     putExtra(INTENT_KEY_WC_SESSION_ID, statefulRPCCall.session.sessionId)
