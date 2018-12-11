47e8b8f41821eada7040cf369dd47a6301d29e35
ligi <ligi@ligi.de>
2018-12-11 12:24:36 +0100

    Let the user know which is the signing account in the subtitle

--- app/src/main/java/org/walleth/activities/SignTextActivity.kt
+++ app/src/main/java/org/walleth/activities/SignTextActivity.kt
@@ -1,6 +1,7 @@
 package org.walleth.activities
 
 import android.app.Activity
+import android.arch.lifecycle.Observer
 import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_sign_text.*
@@ -8,6 +9,7 @@ import org.kethereum.crypto.signMessage
 import org.kethereum.extensions.toHexStringZeroPadded
 import org.koin.android.ext.android.inject
 import org.walleth.R
+import org.walleth.data.AppDatabase
 import org.walleth.data.DEFAULT_PASSWORD
 import org.walleth.data.keystore.WallethKeyStore
 import org.walleth.data.networks.CurrentAddressProvider
@@ -17,29 +19,34 @@ class SignTextActivity : BaseSubActivity() {
 
     private val keyStore: WallethKeyStore by inject()
     private val currentAddressProvider: CurrentAddressProvider by inject()
+    private val appDatabase: AppDatabase by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_sign_text)
 
+        val currentAddress = currentAddressProvider.getCurrent()
+        appDatabase.addressBook.byAddressLiveData(currentAddress).observe(this, Observer { entry ->
+            supportActionBar?.subtitle = "Signing as " + (entry?.name ?: currentAddress.hex)
+        })
+
         val text = intent.getStringExtra(Intent.EXTRA_TEXT)
 
         textToSign.text = text
 
         fab.setOnClickListener {
-            val address = currentAddressProvider.getCurrent()
             val byteArray = text.toByteArray()
             val message = ("\u0019Ethereum Signed Message:\n" + byteArray.size).toByteArray() + byteArray
 
-            val signature = keyStore.getKeyForAddress(address, DEFAULT_PASSWORD)?.signMessage(message)
+            val signature = keyStore.getKeyForAddress(currentAddress, DEFAULT_PASSWORD)?.signMessage(message)
 
             val rHEX = signature?.r?.toHexStringZeroPadded(64, false)
             val sHEX = signature?.s?.toHexStringZeroPadded(64, false)
             val v = signature?.v
 
             val signatureHex = (rHEX + sHEX + v?.toHexString())
-            setResult(Activity.RESULT_OK, Intent().putExtra("SIGNATURE", signatureHex).putExtra("ADDRESS", address.cleanHex))
+            setResult(Activity.RESULT_OK, Intent().putExtra("SIGNATURE", signatureHex).putExtra("ADDRESS", currentAddress.cleanHex))
             finish()
         }
 
