2068471be88798b18b9f5bc884795fd5783e73fe
ligi <ligi@ligi.de>
2019-02-15 19:01:30 +0100

    Make MainActivity default here

--- app/src/debug/java/org/walleth/DebugTrampolin.kt
+++ app/src/debug/java/org/walleth/DebugTrampolin.kt
@@ -3,15 +3,14 @@ package org.walleth
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import org.ligi.kaxt.startActivityFromClass
-import org.walleth.activities.AddressBookActivity
-import org.walleth.activities.CreateTransactionActivity
+import org.walleth.activities.MainActivity
 
 class DebugTrampolin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivityFromClass(AddressBookActivity::class.java)
+        startActivityFromClass(MainActivity::class.java)
         finish()
     }
 }
