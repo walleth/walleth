76d77e3a1c1000a10981d552f9a663faeb7616da
ligi <ligi@ligi.de>
2019-05-12 23:02:42 +0200

    Remove jetifier workaround - not needed anymore after disabling closes #354

--- update-dependency-pinning.sh
+++ update-dependency-pinning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gradle -q calculateChecksums -PnoWitness=kapt -PisCI -PsingleFlavor | grep -v "registerResGeneratingTask is deprecated, use registerGeneratedResFolders(FileCollection)" | grep -v Skipping | grep -v transforms-2 > app/witness.gradle
+gradle -q calculateChecksums -PnoWitness=kapt -PisCI -PsingleFlavor | grep -v "registerResGeneratingTask is deprecated, use registerGeneratedResFolders(FileCollection)" | grep -v Skipping > app/witness.gradle
 
  
