2d2d79e8b9aa6ac349912f6672559f7b29f1cff3
ligi <ligi@ligi.de>
2019-05-19 19:53:56 +0200

    Workaround for #363

--- update-dependency-pinning.sh
+++ update-dependency-pinning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gradle -q calculateChecksums -PnoWitness=kapt -PisCI -PsingleFlavor | grep -v "registerResGeneratingTask is deprecated, use registerGeneratedResFolders(FileCollection)" | grep -v Skipping > app/witness.gradle
+gradle -q calculateChecksums -PnoWitness=kapt -PisCI -PsingleFlavor | grep -v "registerResGeneratingTask is deprecated, use registerGeneratedResFolders(FileCollection)" | grep -v ':android-sdk:' | grep -v ':android:' | grep -v Skipping > app/witness.gradle
 
  
