e6312d94f213cc0feb82f24d7647e8a7b75715f6
ligi <ligi@ligi.de>
2019-05-30 03:07:55 +0200

    Make lint happy

--- app/src/androidTest/java/org/walleth/tests/TheIntentHandlerActivity.kt
+++ app/src/androidTest/java/org/walleth/tests/TheIntentHandlerActivity.kt
@@ -1,6 +1,6 @@
 package org.walleth.tests
 
-import androidx.test.InstrumentationRegistry
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
@@ -9,6 +9,7 @@ import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.ligi.trulesk.TruleskIntentRule
+import org.walleth.App
 import org.walleth.R
 import org.walleth.activities.IntentHandlerActivity
 import org.walleth.activities.getEthereumViewIntent
@@ -21,14 +22,14 @@ class TheIntentHandlerActivity {
 
     @Test
     fun handlesInvalidScannedStrings() {
-        rule.launchActivity(InstrumentationRegistry.getTargetContext().getEthereumViewIntent("Ðereum string"))
+        rule.launchActivity(ApplicationProvider.getApplicationContext<App>().getEthereumViewIntent("The invalid string"))
         Espresso.onView(withText(R.string.create_tx_error_invalid_url_title)).check(matches(isDisplayed()))
         Truth.assertThat(rule.activity.isFinishing).isFalse()
     }
 
     @Test
     fun handlesAddressesWithValue() {
-        rule.launchActivity(InstrumentationRegistry.getTargetContext().getEthereumViewIntent("ethereum:0xdeadbeef?value=100000000000000"))
+        rule.launchActivity(ApplicationProvider.getApplicationContext<App>().getEthereumViewIntent("ethereum:0xdeadbeef?value=100000000000000"))
         Espresso.onView(allOf(withId(R.id.current_eth), isDescendantOfA(withId(R.id.amount_value))))
                 .check(matches(withText("0.0001")))
     }
