a6d359a678c9382c5f60e092217c3746c7e43e99
ligi <ligi@ligi.de>
2019-11-27 12:00:42 +0900

    Replace deprecated method with ViewCompat method

--- app/src/main/java/org/walleth/valueview/ValueView.kt
+++ app/src/main/java/org/walleth/valueview/ValueView.kt
@@ -5,6 +5,7 @@ import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.core.view.ViewCompat
 import kotlinx.android.synthetic.main.value.view.*
 import org.ligi.kaxt.startActivityFromClass
 import org.walleth.R
@@ -40,12 +41,11 @@ open class ValueView(context: Context, attrs: AttributeSet) : LinearLayout(conte
         }
         current_eth.isCursorVisible = allowEdit
         current_eth.isFocusableInTouchMode = allowEdit
-        current_eth.setBackgroundDrawable(null)
+        ViewCompat.setBackground(current_eth, null)
 
         current_fiat.isEnabled = allowEdit
         current_fiat.isFocusableInTouchMode = allowEdit
-        current_fiat.setBackgroundDrawable(null)
-
+        ViewCompat.setBackground(current_fiat, null)
 
         if (showsPrecise) {
             current_token_symbol.setOnClickListener {
