6b9d8b787a67415503932be1dea53deb3216e858
ligi <ligi@ligi.de>
2020-04-24 00:37:53 +0200

    When calling getPasswordForAccountType() with a wrong type fail with the type
    This to get to the bottom of #453

--- app/src/main/java/org/walleth/util/security/PasswordProvider.kt
+++ app/src/main/java/org/walleth/util/security/PasswordProvider.kt
@@ -22,5 +22,5 @@ fun FragmentActivity.getPasswordForAccountType(type: String, callback: (password
         callback(pwd)
     }
     ACCOUNT_TYPE_BURNER -> callback(DEFAULT_PASSWORD)
-    else -> throw IllegalArgumentException("getPasswordForAccountType() must be called for ACCOUNT_TYPE_PIN_PROTECTED, ACCOUNT_TYPE_PASSWORD_PROTECTED or ACCOUNT_TYPE_BURNER")
+    else -> throw IllegalArgumentException("getPasswordForAccountType() must be called for ACCOUNT_TYPE_PIN_PROTECTED, ACCOUNT_TYPE_PASSWORD_PROTECTED or ACCOUNT_TYPE_BURNER but was $type")
 }
