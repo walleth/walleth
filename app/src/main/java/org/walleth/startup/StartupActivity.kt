d139262c9c3dc15a2c72960743d76c1ee06ba465
ligi <ligi@ligi.de>
2021-01-30 20:26:02 +0100

    Migrate StartupActivity to registerForActivityResult

--- app/src/main/java/org/walleth/startup/StartupActivity.kt
+++ app/src/main/java/org/walleth/startup/StartupActivity.kt
@@ -3,6 +3,7 @@ package org.walleth.startup
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import org.kethereum.model.Address
@@ -12,16 +13,29 @@ import org.ligi.kaxt.startActivityFromClass
 import org.ligi.kaxtui.alert
 import org.walleth.accounts.CreateAccountActivity
 import org.walleth.data.EXTRA_KEY_ADDRESS
-import org.walleth.data.REQUEST_CODE_CREATE_ACCOUNT
 import org.walleth.data.addresses.CurrentAddressProvider
 import org.walleth.overview.OverviewActivity
 import org.walleth.startup.StartupStatus.*
-import java.lang.IllegalStateException
 
 class StartupActivity : AppCompatActivity() {
 
     private val currentAddressProvider: CurrentAddressProvider by inject()
     private val viewModel: StartupViewModel by viewModel()
+
+    private val createAccountActionForResult = registerForActivityResult(StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK && it.data != null) {
+
+            val addressString = it.data?.getStringExtra(EXTRA_KEY_ADDRESS) ?: throw IllegalStateException("No EXTRA_KEY_ADDRESS in onActivityResult")
+            val address = Address(addressString)
+            currentAddressProvider.setCurrent(address)
+            startActivityFromClass(OverviewActivity::class.java)
+            finish()
+
+        } else {
+            finish()
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +43,7 @@ class StartupActivity : AppCompatActivity() {
         viewModel.status.observe(this, Observer<StartupStatus> { status ->
             when (status) {
                 is NeedsAddress -> {
-                    startActivityForResult(Intent(this, CreateAccountActivity::class.java), REQUEST_CODE_CREATE_ACCOUNT)
+                    createAccountActionForResult.launch(Intent(this, CreateAccountActivity::class.java))
                 }
                 is HasChainAndAddress -> {
                     startActivityFromClass(OverviewActivity::class.java)
@@ -42,22 +56,5 @@ class StartupActivity : AppCompatActivity() {
                 }
             }
         })
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && data != null) {
-            when (requestCode) {
-                REQUEST_CODE_CREATE_ACCOUNT -> {
-                    val addressString = data.getStringExtra(EXTRA_KEY_ADDRESS) ?: throw IllegalStateException("No EXTRA_KEY_ADDRESS in onActivityResult")
-                    val address = Address(addressString)
-                    currentAddressProvider.setCurrent(address)
-                    startActivityFromClass(OverviewActivity::class.java)
-                    finish()
-                }
-                else -> super.onActivityResult(requestCode, resultCode, data)
-            }
-        } else {
-            finish()
-        }
     }
 }
