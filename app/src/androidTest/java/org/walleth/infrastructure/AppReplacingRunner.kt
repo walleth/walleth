636873c51caffc3b953be31cf40af9384ba9a30a
ligi <ligi@ligi.de>
2021-04-24 07:01:22 +0200

    Fail UI tests early when in online flavor
    wasted so much time realizing the tests fail (in a flaky way) when accidentally still in online mode.

--- app/src/androidTest/java/org/walleth/infrastructure/AppReplacingRunner.kt
+++ app/src/androidTest/java/org/walleth/infrastructure/AppReplacingRunner.kt
@@ -1,8 +1,15 @@
 package org.walleth.infrastructure
 
 import org.ligi.trulesk.AppReplacingRunnerBase
+import org.walleth.BuildConfig
 
 class AppReplacingRunner : AppReplacingRunnerBase() {
+
+    init {
+        if (BuildConfig.FLAVOR_connectivity == "online") {
+            throw IllegalStateException("UI Tests called in online flavor - to prevent upstream pain we fail here fast now. Please test ui in offline flavor.")
+        }
+    }
 
     override fun testAppClass() = TestApp::class.java
 
